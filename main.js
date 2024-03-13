ee1d16a909ce2a8c246182d85bf5475fe95e2c78
Patrick Taschner <patricktaschner.pt@gmai.com>
2024-03-13 21:01:33 +0100

    Added Selection

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 import './style.css';
-import {Map, View} from 'ol';
+import {Map, MapBrowserEvent, View} from 'ol';
 import GeoJSON from 'ol/format/GeoJSON.js';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 import {Style, Stroke, Fill} from 'ol/style';
 import * as olProj from 'ol/proj';
 import {OSM, Vector as VectorSource} from 'ol/source.js';
+import Select from 'ol/interaction/Select.js';
 
 var portugalCoords = olProj.fromLonLat([-8.6100, 39.1496]);
 
+// Portugal Region Source
 const source = new VectorSource({
   url: 'data/portugal.geojson',
   format: new GeoJSON(),
 });
 
+// Portugal Layer and Styling
 const vectorLayer = new VectorLayer({
   source: source,
   style: new Style({
@@ -25,6 +28,7 @@ const vectorLayer = new VectorLayer({
   })
 });
 
+// Center on Portugal
 const view = new View({
   center: portugalCoords,
   zoom: 7
@@ -41,6 +45,7 @@ const map = new Map({
   view: view
 });
 
+// Click Listener for Menu
 window.addEventListener("load", (event) => {
   var el = document.getElementById('districts').addEventListener('click', (e) => {
     var selected = e.target.textContent;
@@ -51,9 +56,25 @@ window.addEventListener("load", (event) => {
       return;
     }
 
-    // const polygon = feature.getGeometry();
-    // view.fit(polygon, {padding: [170, 50, 30, 150]});
     const polygon = feature.getGeometry();
     view.fit(polygon, {padding: [170, 50, 30, 150]});
   })
 });
+
+
+// Highlight selected Region
+let select = new Select(
+  {
+    style: new Style({
+      fill: new Fill({
+        color: '#eeeeee',
+      }),
+      stroke: new Stroke({
+        color: 'rgba(255, 255, 255, 0.7)',
+        width: 2,
+      }),
+    })
+  }
+);
+
+map.addInteraction(select);
